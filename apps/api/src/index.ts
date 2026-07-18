5e92ea7af732f067ae221e79a7bbb1c486e5432a
Aditya Chaudhary <aditya.chaudhary0523@gmail.com>
2026-07-18 23:04:50 +0530

    refactor(workspaces): simplify internal packages

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,9 @@
-import { Hono } from 'hono'
-import { CONSTANT } from '@repo/constants'
+import { Hono } from "hono"
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text(CONSTANT)
+app.get("/", (c) => {
+    return c.text("Hello from your Hono API!")
 })
 
 export default app
